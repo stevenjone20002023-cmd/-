25529049051899314edf322d0be6331da7b76638
stevenjone20002023-cmd <steven.jone.2000.2023@gmail.com>
2026-03-30 11:34:57 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// ملف: service-worker.js
+const CACHE_NAME = 'mask-store-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './config.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
